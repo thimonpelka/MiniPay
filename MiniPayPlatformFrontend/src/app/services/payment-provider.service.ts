19d97f7fac7f1b932b9770309a2bad35297675a4
Thimon Pelka <thimon.pelka@gmail.com>
2025-07-09 09:55:13 +0200

    feat: added new routes to payment provider service

--- MiniPayPlatformFrontend/src/app/services/payment-provider.service.ts
+++ MiniPayPlatformFrontend/src/app/services/payment-provider.service.ts
@@ -1,21 +1,51 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable, Optional } from '@angular/core';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { PaymentProviderDto } from '../dto/paymentProviderDto';
+import { CreatePaymentProviderDto } from '../dto/createPaymentProviderDto';
+import { UpdatePaymentProviderDto } from '../dto/updatePaymentProviderDto';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaymentProviderService {
-  constructor(@Inject("API_URL") private apiUrl: string, @Optional() private httpClient?: HttpClient) { }
+  constructor(
+    @Inject('API_URL') private apiUrl: string,
+    private httpClient: HttpClient,
+  ) { }
 
-  public getPaymentProviders() {
-    if (this.httpClient) {
-      return this.httpClient.get<PaymentProviderDto[]>(`${this.apiUrl}/PaymentProvider`);
-    } else {
-      return of([]);
-    }
+  public getPaymentProviders(): Observable<PaymentProviderDto[]> {
+    return this.httpClient.get<PaymentProviderDto[]>(
+      `${this.apiUrl}/PaymentProvider`,
+    );
   }
 
+  public getPaymentProviderById(id: string): Observable<PaymentProviderDto> {
+    return this.httpClient.get<PaymentProviderDto>(
+      `${this.apiUrl}/PaymentProvider/${id}`,
+    );
+  }
+
+  public createPaymentProvider(
+    provider: CreatePaymentProviderDto,
+  ): Observable<PaymentProviderDto> {
+    return this.httpClient.post<PaymentProviderDto>(
+      `${this.apiUrl}/PaymentProvider`,
+      provider,
+    );
+  }
+
+  public updatePaymentProvider(
+    provider: UpdatePaymentProviderDto,
+  ): Observable<PaymentProviderDto> {
+    return this.httpClient.put<PaymentProviderDto>(
+      `${this.apiUrl}/PaymentProvider/${provider.id}`,
+      provider,
+    );
+  }
+
+  public deletePaymentProvider(id: string): Observable<void> {
+    return this.httpClient.delete<void>(`${this.apiUrl}/PaymentProvider/${id}`);
+  }
 }
