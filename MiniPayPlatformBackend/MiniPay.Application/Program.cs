af2e97e4848eca3c1f32b668760f3b5d18bdd150
Thimon Pelka <thimon.pelka@gmail.com>
2025-07-10 12:40:56 +0200

    doc: added additional comments for better code clarity

--- MiniPayPlatformBackend/MiniPay.Application/Program.cs
+++ MiniPayPlatformBackend/MiniPay.Application/Program.cs
@@ -3,6 +3,7 @@ using MiniPay.Application.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
 
 // Add services to the container.
